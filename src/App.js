4e00eff01cf4f18e652a16283c98178dd145a0bb
JellyFish <kurer456@naver.com>
2022-01-14 17:53:18 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import Note from "./Components/Note";
